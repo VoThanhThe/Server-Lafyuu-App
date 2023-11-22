65f8a45e5c8933758618a638cc9ab32bb12f5dd6
VoThanhThe <113089675+VoThanhThe@users.noreply.github.com>
2023-11-23 00:20:01 +0700

    update upload product lên firebase

--- routes/cpanel/ProductCPanel.js
+++ routes/cpanel/ProductCPanel.js
@@ -6,66 +6,102 @@ const uploadFile = require('../../middle/UploadFile');
 const CONFIG = require('../../config/Config');
 const auth = require('../../middle/Authen');
 
-//http://localhost:3000/cpanel/product/
-router.get('/', [auth.authenWeb], async function(req, res, next) {
-    const products = await productController.getAllProduct();
-    res.render('./product/list',{products});
-  });
-//http://localhost:3000/cpanel/product/:id/delete
-router.get('/:id/delete', [auth.authenWeb], async function(req, res, next) {
-  try {
-    const {id} = req.params;
-    await productController.deleteProductByID(id);
-    res.json({status: true});
-  } catch (error) {
-    res.json({status: false});
-  }
-    
-  });
+// upload image
+const appFirebase = require('../../config/FirebaseConfig');
+const { getStorage, ref, getDownloadURL, uploadBytesResumable } = require("firebase/storage");
+const multer = require("multer");
+// Initialize Cloud Storage and get a reference to the service
+const storage = getStorage();
+const storageRef = ref(storage, 'products/');
 
-  //http://localhost:3000/cpanel/product/new
-  //hien thi trang them moi san pham
-router.get('/new', [auth.authenWeb], async function(req, res, next) {
-  const categories = await categoryController.getAllCategory();
-  res.render('user/form', {categories});
+// Setting up multer as a middleware to grab photo uploads
+const upload = multer({ storage: multer.memoryStorage() });
+
+const giveCurrentDateTime = () => {
+  const today = new Date();
+  const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+  const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  const dateTime = date + ' ' + time;
+  return dateTime;
+}
+
+//http://localhost:3000/cpanel/product/
+router.get('/', [auth.authenWeb], async function (req, res, next) {
+  const products = await productController.getAllProduct();
+  res.render('./product/list', { products });
+});
+//http://localhost:3000/cpanel/product/:id/delete
+router.get('/:id/delete', [auth.authenWeb], async function (req, res, next) {
+  try {
+    const { id } = req.params;
+    await productController.deleteProductByID(id);
+    res.json({ status: true });
+  } catch (error) {
+    res.json({ status: false });
+  }
+
 });
 
-  //http://localhost:3000/cpanel/product/new
-  //xu li them moi san pham
-  router.post('/new', [uploadFile.single('image'),] , async function(req, res, next) {
-    try {
-        //ipconfig
-      let {body, file} = req;
-      if(file){
-        let image = `${CONFIG.CONTANTS.IP}images/${file.filename}`;
-        body = {...body, image: image};
-      }
-      let {name,price,quantity,image,category} = body;
-      console.log(image);
-      await productController.addNewProduct(name,price,quantity,image,category);
-      return res.redirect('/cpanel/product');
-    } catch (error) {
-      console.log('Add new product error: ', error);
-      next(error);
-    }
-  });
-  
+//http://localhost:3000/cpanel/product/new
+//hien thi trang them moi san pham
+router.get('/new', [auth.authenWeb], async function (req, res, next) {
+  const categories = await categoryController.getAllCategory();
+  res.render('user/form', { categories });
+});
 
-  //http://localhost:3000/cpanel/product/
-  //hien thi trang cap nhat san pham
-router.get('/:id/edit', [auth.authenWeb], async function(req, res, next) {
+//http://localhost:3000/cpanel/product/new
+//xu li them moi san pham
+router.post('/new', [upload.single('image'),], async function (req, res, next) {
   try {
-    const {id} = req.params;
+    //ipconfig
+    // let {body, file} = req;
+    // if(file){
+    //   let image = `${CONFIG.CONTANTS.IP}images/${file.filename}`;
+    //   body = {...body, image: image};
+    // }
+    let { name, price, quantity, image, category } = req.body;
+
+    const dateTime = giveCurrentDateTime();
+
+    const storageRef = ref(storage, `products/${req.file.originalname + "       " + dateTime}`);
+
+    // Create file metadata including the content type
+    const metadata = {
+      contentType: req.file.mimetype,
+    };
+
+    // Upload the file in the bucket storage
+    const snapshot = await uploadBytesResumable(storageRef, req.file.buffer, metadata);
+
+    // Grab the public url
+    const downloadURL = await getDownloadURL(snapshot.ref);
+
+    console.log('File successfully uploaded.');
+    // console.log("imageURL:", image);
+    await productController.addNewProduct(name, price, quantity, downloadURL, category);
+    return res.redirect('/cpanel/product');
+  } catch (error) {
+    console.log('Add new product error: ', error);
+    next(error);
+  }
+});
+
+
+//http://localhost:3000/cpanel/product/
+//hien thi trang cap nhat san pham
+router.get('/:id/edit', [auth.authenWeb], async function (req, res, next) {
+  try {
+    const { id } = req.params;
     const product = await productController.getProductByID(id);
     let categories = await categoryController.getAllCategory();
-    for(let index = 0; index < categories.length; index++){
+    for (let index = 0; index < categories.length; index++) {
       const element = categories[index];
       categories[index].selected = false;
-      if(element._id.toString() == product.category.toString()){
+      if (element._id.toString() == product.category.toString()) {
         categories[index].selected = true;
       }
     }
-    res.render('product/edit', {product, categories});
+    res.render('product/edit', { product, categories });
   } catch (error) {
     next(error);
   }
@@ -73,23 +109,23 @@ router.get('/:id/edit', [auth.authenWeb], async function(req, res, next) {
 
 
 //http://localhost:3000/cpanel/product/:id/edit
-  //xu li cap nhat san pham
-  router.post('/:id/edit', [uploadFile.single('image'),] , async function(req, res, next) {
-    try {
-        //cmd >>>>  ipconfig >>>>>>>>> IPv4 Address;
-        let {id} = req.params;
-      let {body, file} = req;
-      if(file){
-        let image = `${CONFIG.CONTANTS.IP}images/${file.filename}`;
-        body = {...body, image: image};
-      }
-      let {name,price,quantity,image,category} = body;
-      await productController.updateProduct(id,name,price,quantity,image,category);
-      return res.redirect('/cpanel/product');
-    } catch (error) {
-      console.log('Update product error: ', error);
-      next(error);
+//xu li cap nhat san pham
+router.post('/:id/edit', [uploadFile.single('image'),], async function (req, res, next) {
+  try {
+    //cmd >>>>  ipconfig >>>>>>>>> IPv4 Address;
+    let { id } = req.params;
+    let { body, file } = req;
+    if (file) {
+      let image = `${CONFIG.CONTANTS.IP}images/${file.filename}`;
+      body = { ...body, image: image };
     }
-  });
+    let { name, price, quantity, image, category } = body;
+    await productController.updateProduct(id, name, price, quantity, image, category);
+    return res.redirect('/cpanel/product');
+  } catch (error) {
+    console.log('Update product error: ', error);
+    next(error);
+  }
+});
 
 module.exports = router;
